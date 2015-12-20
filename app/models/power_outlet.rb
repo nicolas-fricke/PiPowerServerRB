c47db5a095809bfb1d5d7393bf3cc37f78d7b838
Nicolas Fricke <mail@nicolasfricke.de>
2015-12-20 14:34:26 +0100

    Fix: delegation of is_on to Frequency

--- app/models/power_outlet.rb
+++ app/models/power_outlet.rb
@@ -3,10 +3,15 @@ class PowerOutlet < ActiveRecord::Base
   has_and_belongs_to_many :power_outlet_groups
   validates :name, :frequency, presence: true
 
-  delegate :is_on, :is_on=, to: :frequency
+  delegate :is_on, :is_on=, to: :ensured_frequency
   after_save :save_frequency
 
   def save_frequency
     frequency.save if frequency.changed?
+  end
+
+  private
+  def ensured_frequency
+    frequency || Frequency.new
   end
 end
